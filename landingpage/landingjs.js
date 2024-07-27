809071ed9e38daa44ae85f8128a06ffcfa255c44
S-GIRI <168095425+S-GIRI@users.noreply.github.com>
2024-07-27 12:12:19 +0530

    Add files via upload

---
+++ landingpage/landingjs.js
@@ -0,0 +1,6 @@
+// function navigate(pageId) {
+//     document.querySelectorAll('.page-section').forEach(section => {
+//         section.style.display = 'none';
+//     });
+//     document.getElementById(pageId).style.display = 'block';
+// }
